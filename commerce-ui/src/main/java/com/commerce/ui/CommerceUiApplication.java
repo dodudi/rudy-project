4bbf8cffc7afd14e6b1f469e3ff73df60793cf7b
Rudy <idejrdud@gmail.com>
2026-03-22 17:50:40 +0900

    feat: commerce-ui 프로젝트 추가

---
+++ commerce-ui/src/main/java/com/commerce/ui/CommerceUiApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceUiApplication.class, args);
+    }
+
+}
