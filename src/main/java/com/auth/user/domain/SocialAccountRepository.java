f873bb1f2695204d03fdcdc60573d89a3ee32feb
Rudy <idejrdud@gmail.com>
2026-04-14 15:16:32 +0900

    feat: Social 계정 조회 로직을 Fetch Join 로 수정

--- src/main/java/com/auth/user/domain/SocialAccountRepository.java
+++ src/main/java/com/auth/user/domain/SocialAccountRepository.java
@@ -1,11 +1,16 @@
 package com.auth.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface SocialAccountRepository extends JpaRepository<SocialAccount, UUID> {
 
-    Optional<SocialAccount> findByProviderAndProviderId(SocialProvider provider, String providerId);
+    @Query("SELECT sa FROM SocialAccount sa JOIN FETCH sa.user u LEFT JOIN FETCH u.roles WHERE sa.provider = :provider AND sa.providerId = :providerId")
+    Optional<SocialAccount> findByProviderAndProviderId(
+            @Param("provider") SocialProvider provider,
+            @Param("providerId") String providerId);
 }
